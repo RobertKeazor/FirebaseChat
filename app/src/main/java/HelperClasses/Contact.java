70031b6170e4cf366f97b0a9c4f46f1d9f71490a
Robert Keazor <Rkeazor@gmail.com>
2015-10-10 15:20:04 -0400

    Database Class Created

---
+++ app/src/main/java/HelperClasses/Contact.java
@@ -0,0 +1,40 @@
+package HelperClasses;
+
+
+public class Contact {
+
+    private String UserName;
+    private String password;
+    private String Email;
+
+
+    public Contact(String userName, String password, String email) {
+        UserName = userName;
+        this.password = password;
+        Email = email;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
---
+++ app/src/main/java/HelperClasses/ContactDatabase.java
@@ -0,0 +1,47 @@
+package HelperClasses;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+
+public class ContactDatabase extends SQLiteOpenHelper {
+
+
+    public static final int DatabaseEntries = 1;
+    public static final String Database_NAME = "Contacts.db";
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +" )";
+
+
+    public ContactDatabase(Context context) {
+        super(context, Database_NAME, null, DatabaseEntries);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public static abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+
+    }
+
+}
+
+
